8bd3e7b4d807f50542ff306be5a01795d3e477d6
BoxOfGos <58175042+BoxOfGos@users.noreply.github.com>
2021-03-16 17:45:29 -0500

    Box selection

--- PokemonCombatEvolved/Assets/Editor/AbilityNode.cs
+++ PokemonCombatEvolved/Assets/Editor/AbilityNode.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 
 public class AbilityNode
 {
+    public AbilityTreeEditor editor;
     public string name;
     public string description;
     public Rect rect;
 
-    public AbilityTreeEditor editor;
 
     public AbilityNode(AbilityTreeEditor editor, Vector2 position)
     {
@@ -29,32 +29,66 @@ public class AbilityNode
         GUILayout.BeginVertical();
         GUILayout.Label(description);
         GUILayout.EndVertical();
+    }
 
-        Event e = Event.current;
-
+    public void ProcessEvents(Event e)
+    {
         switch(e.type)
         {
             case EventType.MouseDown:
+                editor.selectionFlag = false;
+
                 if (e.shift)
                 {
                     if (!editor.selectedNodes.Contains(this))
+                    {
                         editor.selectedNodes.Add(this);
-                    else editor.selectedNodes.Remove(this);
+                        editor.selectionFlag = true;
+                    }
+                    else if (editor.selectedNodes.Count != 0)
+                    {
+                        if (editor.selectedNodes[editor.selectedNodes.Count - 1] != this)
+                        {
+                            editor.selectedNodes.Remove(this);
+                            editor.selectedNodes.Add(this);
+                            editor.selectionFlag = true;
+                        }
+                    }
                 }
                 else
-                    editor.selectedNodes = new List<AbilityNode> { this };
-
-                if (e.button == 1)
                 {
-                    GenericMenu myMenu = new GenericMenu();
-                    myMenu.AddItem(new GUIContent("Delete"), false, () => editor.DeleteNode(this));
-                    myMenu.ShowAsContext();
-                    e.Use();
+                    editor.selectedNodes = new List<AbilityNode>() { this };
+                    editor.selectionFlag = true;
                 }
+
+                switch (e.button)
+                {
+                    case 0:
+                        editor.draggedNode = this;
+                        break;
+                    case 1:
+                        GenericMenu myMenu = new GenericMenu();
+                        myMenu.AddItem(new GUIContent("Delete node"), false, () => editor.DeleteNode(this));
+                        myMenu.ShowAsContext();
+                        break;
+                }
+
+                e.Use();
                 GUI.changed = true;
                 break;
-        }
 
-        GUI.DragWindow();
+            case EventType.MouseUp:
+                if (editor.draggedNode != null)
+                {
+                    if (editor.draggedDistance < 5 && !editor.selectionFlag)
+                        editor.selectedNodes.Remove(editor.draggedNode);
+
+                    editor.draggedNode = null;
+                    editor.draggedDistance = 0f;
+
+                    GUI.changed = true;
+                }
+                break;
+        }
     }
 }
--- PokemonCombatEvolved/Assets/Editor/AbilityTreeEditor.cs
+++ PokemonCombatEvolved/Assets/Editor/AbilityTreeEditor.cs
@@ -6,7 +6,8 @@ using System.IO;
 
 public class AbilityTreeEditor : EditorWindow
 {
-    public float gridSize = 2000f;
+    public int gridSize = 2000;
+    public int gridStep = 100;
     public Vector2 gridScroll = new Vector2();
     public Rect gridRect;
     public Vector2 dataScroll = new Vector2();
@@ -15,6 +16,16 @@ public class AbilityTreeEditor : EditorWindow
     public string treeName = "My Ability Tree";
     public List<AbilityNode> nodes = new List<AbilityNode>();
     public List<AbilityNode> selectedNodes = new List<AbilityNode>();
+    public AbilityNode draggedNode = null;
+    public Vector2 mouseDownLocation = new Vector2();
+    public Vector2 mouseDragLocation = new Vector2();
+
+    // EVENT HANDLING PARAMETERS
+    // The distance the draggedNode has ben dragged
+    public float draggedDistance;
+    // If true, last mouse down event selected a node
+    public bool selectionFlag = false;
+    public bool dragBoxFlag = false;
 
     [MenuItem("Window/Pokemon/AbilityTreeEditor")]
     public static void ShowEditor()
@@ -34,7 +45,7 @@ public class AbilityTreeEditor : EditorWindow
 
         // The node grid layout
         gridScroll = EditorGUILayout.BeginScrollView(gridScroll, true, true);
-        DrawNodeGrid(2000, 100);
+        DrawNodeGrid();
         EditorGUILayout.EndScrollView();
         gridRect = GUILayoutUtility.GetLastRect();
 
@@ -63,7 +74,7 @@ public class AbilityTreeEditor : EditorWindow
 
         if (GUI.Button(new Rect(10, 90, 200, 30), new GUIContent("Test button")))
         {
-            Debug.Log(selectedNodes.Count);
+            
         }
 
         DrawMap(120);
@@ -79,18 +90,20 @@ public class AbilityTreeEditor : EditorWindow
         if (GUI.changed) Repaint();
     }
 
-    private void DrawMap(float size)
+    // ONGUI DRAWING METHODS
+    private void DrawMap(float mapSize)
     {
-        Vector2 mapSize = new Vector2(size, size);
-        GUILayout.BeginArea(new Rect(5, position.height - size - 20, size, size));
+        Texture circle = Resources.Load<Texture>("Editor/circle");
+
+        GUILayout.BeginArea(new Rect(5, position.height - mapSize - 20, mapSize, mapSize));
         GUI.backgroundColor = new Color(0, 0, 0, 0.5f);
-        GUI.Box(new Rect(0, 0, size, size), "");
+        GUI.Box(new Rect(0, 0, mapSize, mapSize), "");
         GUI.backgroundColor = new Color(1, 1, 1, 0.5f);
-        GUI.Box(new Rect(2, 2, size - 4, size - 4), "");
+        GUI.Box(new Rect(2, 2, mapSize - 4, mapSize - 4), "");
+
         GUI.backgroundColor = Color.clear;
         for (int i = 0; i < nodes.Count; i++)
         {
-
             int selectedIndex = selectedNodes.IndexOf(nodes[i]);
             if (selectedIndex > -1)
             {
@@ -100,15 +113,223 @@ public class AbilityTreeEditor : EditorWindow
             }
             else GUI.contentColor = Color.black;
 
-
             Vector2 nodePosition = new Vector2
-                (nodes[i].rect.position.x / 2000f * size, nodes[i].rect.position.y / 2000f * size);
-            GUI.Box(new Rect(nodePosition.x, nodePosition.y, 13, 13), (Texture)Resources.Load("Editor/circle"));
+                (nodes[i].rect.position.x / gridSize * mapSize, nodes[i].rect.position.y / gridSize * mapSize);
+            GUI.Box(new Rect(nodePosition.x, nodePosition.y, 13, 13), circle);
         }
+
         GUI.contentColor = GUI.backgroundColor = Color.white;
         GUILayout.EndArea();
     }
 
+    private void DrawNodeGrid()
+    {
+        GUILayout.Box("", GUILayout.Width(gridSize), GUILayout.Height(gridSize));
+
+        Handles.BeginGUI();
+        Handles.color = Color.black;
+        for (int i = 0; i < gridSize / gridStep; i++)
+        {
+            Handles.DrawLine(new Vector3(0, gridStep * i, 0), new Vector3(gridSize, gridStep * i, 0));
+            Handles.DrawLine(new Vector3(gridStep * i, 0, 0), new Vector3(gridStep * i, gridSize, 0));
+        }
+        Handles.EndGUI();
+
+
+        BeginWindows();
+        for (int i = 0; i < nodes.Count; i++)
+        {
+            AbilityNode node = nodes[i];
+
+            int selectedIndex = selectedNodes.IndexOf(node);
+            if (selectedIndex > -1)
+            {
+                if (selectedIndex == selectedNodes.Count - 1)
+                    GUI.color = Color.green;
+                else GUI.color = Color.yellow;
+            }
+            else GUI.color = Color.white;
+
+            node.Draw(i);
+        }
+        GUI.color = Color.white;
+        EndWindows();
+
+
+        if (dragBoxFlag)
+        {
+            Vector2 corner0, corner1;
+
+            corner0 = mouseDownLocation + Vector2.right * (mouseDragLocation - mouseDownLocation).x;
+            corner1 = mouseDownLocation + Vector2.up * (mouseDragLocation - mouseDownLocation).y;
+
+            Handles.BeginGUI();
+            Handles.DrawDottedLines(new Vector3[]
+            {
+                mouseDownLocation, corner0,
+                mouseDownLocation, corner1,
+                mouseDragLocation, corner0,
+                mouseDragLocation, corner1
+            }, 2f);
+            Handles.EndGUI();
+        }
+    }
+
+    private void DrawNodeData()
+    {
+        if (selectedNodes.Count > 0)
+        {
+            AbilityNode dataNode = selectedNodes[selectedNodes.Count - 1];
+
+            EditorGUILayout.BeginVertical();
+
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("Name", GUILayout.Width(100));
+            dataNode.name = EditorGUILayout.DelayedTextField(dataNode.name);
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("Description", GUILayout.Width(100));
+            dataNode.description = EditorGUILayout.DelayedTextField(dataNode.description);
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.EndVertical();
+        }
+        else
+        {
+            EditorGUILayout.LabelField("Select a node");
+        }
+    }
+
+    // EVENT METHODS
+    private void ProcessEvents(Event e)
+    {
+        if (gridRect.Contains(e.mousePosition))
+        {
+            foreach(AbilityNode node in nodes)
+            {
+                Rect nodeRect = new Rect(
+                    node.rect.position.x - gridScroll.x, node.rect.position.y - gridScroll.y,
+                    node.rect.width, node.rect.height);
+
+                if (nodeRect.Contains(e.mousePosition))
+                {
+                    node.ProcessEvents(e);
+                    break;
+                }
+            }
+
+
+            switch(e.type)
+            {
+                case EventType.MouseDown:
+                    switch(e.button)
+                    {
+                        case 0:
+                            BeginBoxDrag(e);
+                            break;
+
+                        case 1:
+                            ShowGridContextMenu(e);
+                            break;
+                    }
+                    break;
+
+                case EventType.MouseDrag:
+                    switch(e.button)
+                    {
+                        case 0:
+                            if (draggedNode != null)
+                                DragNodes(e);
+                            else
+                                UpdateBoxDrag(e);
+                            break;
+
+                        case 2:
+                            DragGridScroll(e);
+                            break;
+                    }
+                    break;
+
+                case EventType.MouseUp:
+                    if (dragBoxFlag)
+                        EndBoxDrag(e);
+                    break;
+            }
+        }
+    }
+
+    private void EndBoxDrag(Event e)
+    {
+        Rect boxRect = new Rect(mouseDownLocation, mouseDragLocation - mouseDownLocation);
+
+        if (!e.shift)
+            selectedNodes = new List<AbilityNode>();
+
+        foreach (AbilityNode node in nodes)
+        {
+            if (boxRect.Overlaps(node.rect, true))
+            {
+                selectedNodes.Remove(node);
+                selectedNodes.Add(node);
+            }
+        }
+
+        dragBoxFlag = false;
+
+        GUI.changed = true;
+        e.Use();
+    }
+
+    private void DragGridScroll(Event e)
+    {
+        gridScroll -= e.delta;
+
+        GUI.changed = true;
+        e.Use();
+    }
+
+    private void UpdateBoxDrag(Event e)
+    {
+        mouseDragLocation = gridScroll + e.mousePosition;
+
+        GUI.changed = true;
+        e.Use();
+    }
+
+    private void DragNodes(Event e)
+    {
+        draggedDistance += e.delta.magnitude;
+
+        foreach (AbilityNode node in selectedNodes)
+            node.rect = new Rect(
+                node.rect.x + e.delta.x, node.rect.y + e.delta.y,
+                node.rect.width, node.rect.height);
+
+        GUI.changed = true;
+        e.Use();
+    }
+
+    private void BeginBoxDrag(Event e)
+    {
+        mouseDownLocation = mouseDragLocation = gridScroll + e.mousePosition;
+        dragBoxFlag = true;
+
+        GUI.changed = true;
+        e.Use();
+    }
+
+    private void ShowGridContextMenu(Event e)
+    {
+        GenericMenu myMenu = new GenericMenu();
+        myMenu.AddItem(new GUIContent("New node"), false, () => CreateNewNode(e.mousePosition + gridScroll));
+        myMenu.ShowAsContext();
+
+        GUI.changed = true;
+        e.Use();
+    }
+
+    // BUTTON AND OTHER METHODS
     private void SaveTree()
     {
         AbilityTreeSerializable dataTree = new AbilityTreeSerializable(this);
@@ -158,90 +379,6 @@ public class AbilityTreeEditor : EditorWindow
                     name = tree.nodeNames[i],
                     description = tree.nodeDescriptions[i]
                 });
-            }
-        }
-    }
-
-    // Draws the grid and all the nodes inside the currently open ability tree
-    private void DrawNodeGrid(int scrollSize, int gridSize)
-    {
-        GUILayout.Box("", GUILayout.Width(scrollSize), GUILayout.Height(scrollSize));
-
-        Handles.BeginGUI();
-        Handles.color = Color.black;
-        for (int i = 0; i < scrollSize / gridSize; i++)
-        {
-            Handles.DrawLine(new Vector3(0, gridSize * i, 0), new Vector3(scrollSize, gridSize * i, 0));
-            Handles.DrawLine(new Vector3(gridSize * i, 0, 0), new Vector3(gridSize * i, scrollSize, 0));
-        }
-        Handles.EndGUI();
-
-
-        BeginWindows();
-        for (int i = 0; i < nodes.Count; i++)
-        {
-            AbilityNode node = nodes[i];
-
-            int selectedIndex = selectedNodes.IndexOf(node);
-            if (selectedIndex > -1)
-            {
-                if (selectedIndex == selectedNodes.Count - 1)
-                    GUI.color = Color.green;
-                else GUI.color = Color.yellow;
-            }
-            else GUI.color = Color.white;
-
-            node.Draw(i);
-        }
-        GUI.color = Color.white;
-        EndWindows();
-    }
-
-    private void DrawNodeData()
-    {
-        if (selectedNodes.Count > 0)
-        {
-            AbilityNode dataNode = selectedNodes[selectedNodes.Count - 1];
-
-            EditorGUILayout.BeginVertical();
-
-            EditorGUILayout.BeginHorizontal();
-            EditorGUILayout.LabelField("Name", GUILayout.Width(100));
-            dataNode.name = EditorGUILayout.DelayedTextField(dataNode.name);
-            EditorGUILayout.EndHorizontal();
-
-            EditorGUILayout.BeginHorizontal();
-            EditorGUILayout.LabelField("Description", GUILayout.Width(100));
-            dataNode.description = EditorGUILayout.DelayedTextField(dataNode.description);
-            EditorGUILayout.EndHorizontal();
-
-            EditorGUILayout.EndVertical();
-        }
-    }
-
-    private void ProcessEvents(Event e)
-    {
-        if (gridRect.Contains(e.mousePosition))
-        {
-            switch(e.type)
-            {
-                case EventType.MouseDown:
-                    if (e.button == 1)
-                    {
-                        GenericMenu myMenu = new GenericMenu();
-                        myMenu.AddItem(new GUIContent("New node"), false, () => CreateNewNode(e.mousePosition + gridScroll));
-                        myMenu.ShowAsContext();
-                        e.Use();
-                    }
-                    break;
-                case EventType.MouseDrag:
-                    if (e.button == 0)
-                    {
-                        gridScroll -= e.delta;
-                        GUI.changed = true;
-                        e.Use();
-                    }
-                    break;
             }
         }
     }
